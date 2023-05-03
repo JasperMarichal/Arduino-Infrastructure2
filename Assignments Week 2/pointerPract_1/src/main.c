f69c87ebb50cf403bca07d85035263d8dd4ef9d8
Jasper_Marichal <jasper.marichal@student.kdg.be>
2023-05-03 14:38:49 +0200

    Added new exercises + Demos

---
+++ Assignments Week 2/pointerPract_1/src/main.c
@@ -0,0 +1,27 @@
+#define MAX_INT 5
+#define SEVENFOLD 7
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  int array[] = { 0, 0 ,0 ,0, 0 };
+      printArray(array);
+      makeArray(array);
+      printArray(array);
+      printf("\n");
+
+      return 0;
+}
+
+void printArray(int array[MAX_INT]) {
+    printf("Content of array:\n");
+    for (int i = 0; i < MAX_INT; i++) {
+        printf("Address: %p has value: %d\n", &array[i], array[i]);   
+    }
+}
+
+void makeArray(int array[]) {
+    for (int i = 0; i < MAX_INT; i++) {
+        array[i] = i * SEVENFOLD;
+    }
+}
---
+++ Assignments Week 2/stringPract_2/src/main.c
@@ -0,0 +1,49 @@
+#define MAX_STRING 7
+#define NAME_LENGTH 10
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+  char names[MAX_STRING][NAME_LENGTH] = { "Lucky", "Wout", "Ard", "Martin", "Sean", "Anton", "Tjalma"};
+    printFirstLetters(names);
+    printf("\n");
+    printLastLetters(names);
+    
+    char *smallest = searchShortestName(names);
+    printf("\nShortest = %s\n", smallest);
+
+    return 0;
+}
+
+void printFirstLetters(char string[MAX_STRING][NAME_LENGTH])
+{
+    printf("First letters: ");
+    for (int i = 0; i < MAX_STRING; i++) {
+        printf("%c", string[i][0]);
+    }
+}
+
+void printLastLetters(char string[MAX_STRING][NAME_LENGTH]) 
+{
+  printf("Last letters: ");
+  for (int i = 0; i < MAX_STRING; i++) {
+      printf("%c", string[i][strlen(string[i]) - 1]);
+    }
+}
+
+char *searchShortestName(char string[MAX_STRING][NAME_LENGTH]) 
+{
+
+  int shortest = strlen(string[0]); 
+  int index = 0;
+
+  for (int i = 1; i < MAX_STRING; i++) {
+    int current = strlen(string[i]);
+    if (current < shortest) {
+      shortest = current;
+      index = i;
+    }
+  }
+
+  return string[index];
+}
